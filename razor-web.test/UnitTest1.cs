019ed853d2d283eb94795b1cb4b55f6d2369b2bf
Pichalie <pichalie@gmail.com>
2025-06-22 10:48:06 +0700

    test

--- razor-web.test/UnitTest1.cs
+++ razor-web.test/UnitTest1.cs
@@ -1,12 +1,20 @@
+using razor_web.Pages;
+
 namespace razor_web.test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void canPlusNumber()
         {
-            bool result = true;
-            Assert.False(result, "1 should not be prime");
+            int razor = new MyCalculator().addnumber(1,1);
+            Assert.Equal(2, razor);
+        }
+        [Fact]
+        public void canMinusNumber()
+        {
+            int razor = new MyCalculator().minusnumber(1, 1);
+            Assert.Equal(0, razor);
         }
     }
 }
---
+++ razor-web/Pages/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace razor_web.Pages
+{
+    public class MyCalculator
+    {
+        public int addnumber(int a, int b)
+        {
+            return a + b;
+        }
+        public int minusnumber(int a, int b)
+        {
+            return a - b + 1;
+        }
+
+    }
+}
